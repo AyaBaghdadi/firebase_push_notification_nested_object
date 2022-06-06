73e1e9cbd9b63f2a041061f28d2a1453ed0d9305
Aya <ayabaghdadi83@yahoo.com>
2022-06-06 21:32:32 +0200

    Initial

---
+++ Technicalisto_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//
+//  Created by Technicalisto.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    
+}
+
+
